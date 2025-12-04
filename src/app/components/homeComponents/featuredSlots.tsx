8c8810cc03849b8eb0d932b4818825ffdc559c03
Robert Porchas <robertgeomaing@gmail.com>
2025-12-03 19:58:50 -0800

    feat: added shadow and rounded featured slot displays

--- src/app/components/homeComponents/featuredSlots.tsx
+++ src/app/components/homeComponents/featuredSlots.tsx
@@ -73,6 +73,7 @@ function FeaturedSlots() {
                     hidden: { opacity: 0, y: 10 },
                     show: { opacity: 1, y: 0, transition: { duration: 0.4 } },
                 }}
+                className="shadow-md rounded-xl"
                 >
                 <Product
                     tag={
